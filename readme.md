03d66d432ee5e0e4b488245c27811242b8b04524
chakib belgaid <chakib.belgaid@gmail.com>
2024-06-04 14:44:25 +0200

    remove : test.py

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-#Comfyui_style
+# Comfyui_style
 
 A simple comfyui_pluging to importe A1111 csv styles into comfyui promot,
 
--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 from util import *
+style_name = "Enhance"
+positive_prompt = "Enter positive prompt here"
 
 styles = load_all_styles()
-styles = list(styles.keys())
-print(styles)
+
+if "{prompt}" in styles[style_name]["positive"]:
+    positive_prompt = styles[style_name]["positive"].replace(
+        '{prompt}', "of " + positive_prompt)
+else:
+    positive_prompt = styles[style_name]["positive"] + \
+        ", " + positive_prompt
+
+print(positive_prompt)
